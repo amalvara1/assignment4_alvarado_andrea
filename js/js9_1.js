7d4abb653b12a29a0c98ef6e4f8544aa7035b367
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-04-02 16:17:48 -0400

    updated window.alert

--- js/js9_1.js
+++ js/js9_1.js
@@ -1,3 +1,2 @@
-var me_div = document.getElementById("div1");
-
-window.alert("the title of this div is " + me_div.title);
+var help_div = document.getElementById("div1");
+window.alert("the title of this div is " + help_div.title);
