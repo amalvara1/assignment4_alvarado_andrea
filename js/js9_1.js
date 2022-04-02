2eebda0c5b69317dea74f090d7777dcf4889a202
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-04-02 16:38:20 -0400

    added a new div by using js

--- js/js9_1.js
+++ js/js9_1.js
@@ -1,2 +1,8 @@
 var help_div = document.getElementById("div1");
 window.alert(help_div.title);
+
+var inner_div = document.createElement("div1");
+var inner_div_text = document.createTextNode("See you!")
+inner_div.appendChild(inner_div_text);
+inner_div.title = "Lucky!";
+me_div.appendChild(inner_div);
