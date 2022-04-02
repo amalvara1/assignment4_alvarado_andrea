b70a3d837b0d0df888af06652591707901ec61a3
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-04-02 17:00:53 -0400

    updated div child in 9_1.js

--- js/js9_1.js
+++ js/js9_1.js
@@ -1,8 +1,7 @@
 var help_div = document.getElementById("div1");
-window.alert(help_div.title);
-
+window.alert(help_div.title)
 var inner_div = document.createElement("div1");
-var inner_div_text = document.createTextNode("See you!")
+var inner_div_text = document.createTextNode("See you!");
 inner_div.appendChild(inner_div_text);
 inner_div.title = "Lucky!";
-me_div.appendChild(inner_div);
+help_div.appendChild(inner_div);
