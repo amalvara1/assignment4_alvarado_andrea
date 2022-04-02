0f6e44e0c17674eae3e4f18a1a08d19366c08620
Andrea Alvarado <a.alvarado1@ufl.edu>
2022-04-02 17:05:37 -0400

    added body elements for 9_2.html

--- js/js9_1.js
+++ js/js9_1.js
@@ -1,5 +1,5 @@
 var help_div = document.getElementById("div1");
-window.alert(help_div.title)
+window.alert(help_div.title);
 var inner_div = document.createElement("div1");
 var inner_div_text = document.createTextNode("See you!");
 inner_div.appendChild(inner_div_text);
